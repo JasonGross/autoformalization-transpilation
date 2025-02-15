21c0eb65634a571ea17aa4e474bd9f5df2dc34ea
Jacob Green <jacob.hugo.green@gmail.com>
2025-02-15 10:36:26 +0000

    Transpilation scorer scaffold.

--- src/scorers/transpilation.py
+++ src/scorers/transpilation.py
@@ -8,7 +8,22 @@ from inspect_ai.scorer import (
 )
 from inspect_ai.solver import TaskState
 
+from main import (
+    DEFINITION_PAIRS,
+    LeanFile,
+    generate_and_prove_iso,
+    import_to_coq,
+    lean_to_coq,
+)
 from tools.itp import run_lean_str
+
+
+class LeanError(Exception):
+    pass
+
+
+class ModelResponseError(Exception):
+    pass
 
 
 @scorer(metrics=[accuracy()])
@@ -24,5 +39,43 @@ def lean_runs_scorer():
         return Score(
             value=CORRECT if correct else INCORRECT,
         )
+
+    return score
+
+
+@scorer(metrics=[accuracy()])
+def transpilation_scorer():
+    async def score(state: TaskState, target: Target | None):
+        try:
+            # extract lean code
+            answer = state.output.completion
+            answer = answer[answer.find("```lean") + 7 : answer.rfind("```")]
+            if not answer:
+                raise ModelResponseError("Unable to find lean code in model response.")
+
+            # compile
+            result = run_lean_str(answer)
+            if result["status"] != 0:
+                raise LeanError(f"Error compiling Lean code: {result['stderr']}")
+
+            # import to Coq
+            lean_file = LeanFile(answer)
+            success, cc_identifiers, error = lean_to_coq(
+                lean_file,
+                DEFINITION_PAIRS,  # TODO: Replace?
+            )
+            if not success:
+                raise LeanError(f"Error importing lean to Coq: {error}")
+
+            # generate and prove isos - (still being worked on)
+            result, errors = generate_and_prove_iso(cc_identifiers)
+            if not result:
+                raise LeanError(f"Error generating and proving iso: {errors}")
+
+            return Score(
+                value=CORRECT,
+            )
+        except Exception as e:
+            return Score(value=INCORRECT, explanation=str(e))
 
     return score
