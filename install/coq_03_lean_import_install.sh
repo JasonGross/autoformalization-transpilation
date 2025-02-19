a41aeff4b32ea582b15ce185ca8cb0a26325f3af
Jason Gross <jgross@mit.edu>
2025-02-19 21:19:30 +0000

    Attempt to fix coq-lean-import

--- install/coq_03_lean_import_install.sh
+++ install/coq_03_lean_import_install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-opam pin add -yn 'git+https://github.com/coq-community/rocq-lean-import.git'
+opam pin add -yn rocq-lean-import
 
-opam install -y coq coq-lean-import
+opam install -y coq rcoq-lean-import
 
 # Allow for editing of coq-lean-import more easily
 cd /root
-ln -s "$(opam var coq-lean-import:build)" coq-lean-import
+ln -s "$(opam var rcoq-lean-import:build)" coq-lean-import
