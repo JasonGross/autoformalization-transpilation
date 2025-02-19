3308e25e62e51f6920fa909df6cce5a818eaf447
Jason Gross <jgross@mit.edu>
2025-02-18 20:24:58 -0800

    Bump version of coq-lean-import (#74)
    Now that https://github.com/coq-community/rocq-lean-import/pull/23 is merged

--- install/coq_03_lean_import_install.sh
+++ install/coq_03_lean_import_install.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 set -e
 
-opam pin add -yn 'git+https://github.com/JasonGross/coq-lean-import.git#lean4-rebased'
+opam pin add -yn 'git+https://github.com/coq-community/rocq-lean-import.git'
 
 opam install -y coq coq-lean-import
 
 # Allow for editing of coq-lean-import more easily
 cd /root
 ln -s "$(opam var coq-lean-import:build)" coq-lean-import
-
-# Setup Coq import
-cd /root/autoformalization
-cat << EOF > target.v
-From LeanImport Require Import Lean.
-Redirect "target.log" Lean Import "target.out".
-EOF
