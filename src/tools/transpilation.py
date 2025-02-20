6c134ed97fc497029cfc5cd2bd38cd6a413e111c
Jason Gross <jgross@mit.edu>
2025-02-20 05:12:26 +0000

    Fix another bug

--- src/tools/transpilation.py
+++ src/tools/transpilation.py
@@ -254,6 +254,7 @@ def transpilation_tool(
     _, init_coq_project = CoqProject.read(iso_checker_path).clean()
     # set some dummy files so that the makefile doesn't fail
     init_coq_project["Isomorphisms.v"] = CoqFile("")
+    init_coq_project["Interface.v"] = CoqFile("")
     if init_coq_targets is not None:
         if isinstance(init_coq_targets, str):
             init_coq_targets = [init_coq_targets]
