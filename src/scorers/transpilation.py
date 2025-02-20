cc806d4c81e07242f6480ae60bbda8d486fdb903
Vishesh Saraswat <67602359+vishesh312@users.noreply.github.com>
2025-02-21 04:38:05 +0530

    added scorer for Checker.vo compilation and number of completed iso proofs

--- src/scorers/transpilation.py
+++ src/scorers/transpilation.py
@@ -88,7 +88,71 @@ def lean_compiles_scorer():
         p_state: ProjectState = store.get("translation_state")
         if p_state is None:
             return Score(value=INCORRECT, explanation="No translation state found")
-        if p_state["result"]["failure_phase"] == CompilationPhase.COMPILATION:
+        if p_state["result"].get("failure_phase") == CompilationPhase.COMPILATION:
             return Score(value=INCORRECT, explanation="Lean code does not compile")
         return Score(value=CORRECT)
+    return score
+
+@scorer(metrics=[accuracy()])
+def checker_compiles_scorer():
+    """Checks if Checker.vo compiles and check print assumptions output"""
+    async def score(state: TaskState, target: Target | None):
+        store = inspect_ai.util.store()
+        p_state: ProjectState = store.get("translation_state")
+        if p_state is None:
+            return Score(value=INCORRECT, explanation="No translation state found")
+        result = p_state["result"]
+        if result.get("failure_phase") == CompilationPhase.COMPILATION:
+            return Score(value=INCORRECT, explanation="Lean code does not compile")
+
+        coq_project = p_state.get("coq_project")
+        if not coq_project:
+            return Score(value=INCORRECT, explanation="No Coq project state found")
+        compilation_result, _ = coq_project.make("Checker.vo", check=False)
+        
+        if compilation_result.returncode != 0:
+            return Score(
+                value=INCORRECT, 
+                explanation=f"Checker.vo failed to build:\n{compilation_result.stderr}"
+            )
+        if "Axioms:" in compilation_result.stdout:
+            return Score(
+                # @Jason change to INCORRECT?
+                value=PARTIAL,
+                explanation="Compilation succeeded but has axioms in assumptions"
+            )
+        return Score(value=CORRECT, explanation="Checker.vo compiled successfully with no axioms")
+
+    return score
+
+@scorer(metrics=[accuracy()])
+def isos_proven_scorer():
+    """score based on how many isos were proven"""
+    async def score(state: TaskState, target: Target | None):
+        store = inspect_ai.util.store()
+        p_state: ProjectState = store.get("translation_state")
+        if p_state is None:
+            return Score(value=INCORRECT, explanation="No translation state found")
+        result = p_state["result"]
+        if result.get("failure_phase") == CompilationPhase.COMPILATION:
+            return Score(value=INCORRECT, explanation="Lean code does not compile")
+        blocks = p_state.get("cc_identifiers_blocks", [])
+        if not blocks:
+            return Score(value=INCORRECT, explanation="No blocks found")
+        total_isos, proven_isos = 0, 0
+        for block in blocks:
+            if isinstance(block, str):
+                continue
+            proof = block[2]
+            if proof is not None:
+                total_isos += 1
+                if "Admitted" not in proof:
+                    proven_isos += 1
+
+        if total_isos == 0:
+            return Score(value=INCORRECT, explanation="No isomorphism proofs found")
+        return Score(
+            value=proven_isos / total_isos,
+            explanation=f"{proven_isos}/{total_isos} isomorphism proofs were proven"
+        )
     return score
